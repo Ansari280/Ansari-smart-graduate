66514d8c07fef1725d0a295862f42b40865bfd9f
Ansari280 <112820589+Ansari280@users.noreply.github.com>
2022-09-09 19:33:04 +0100

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+printf("Welcome to C Programing");
+int Age=20;
+char name[10]="mohamed";
+printf("\n my nameis.......%s",name);
+printf("\t my age is ....%d",Age);
+char c='a';
+char cc=100;
+const double pi=3.14;
+printf("/t My pi value is ...%f",pi);
+printf("/n the size of  pi  variable..%d",sizeof(pi));
+}
